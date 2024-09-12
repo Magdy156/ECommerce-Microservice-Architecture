204ae4cad41758b42daf882048fa9fb18f2927a0
Magdy156 <magdyhamdy156@gmail.com>
2024-09-12 17:32:44 +0300

    api-gateway

---
+++ api-gateway/src/main/java/com/example/ecommerce/api_gateway/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.api_gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter{
+    
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        
+        logger.info("path of the request recieved -> {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
+
+
+
